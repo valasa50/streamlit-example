c8c88852bfbe094e0d7268f7862e130203bc5edd
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-19 23:44:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,40 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+import streamlit
 
-"""
-# Welcome to Streamlit!
+streamlit.title('My Parents New Healthy Diner')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+streamlit.header('Breakfast Favorite')
+streamlit.text('\N{flexed biceps} Omega 3 & Blueberry Oatmeal')
+streamlit.text('\N{flexed biceps} Kale, Spinach & Rocket Smoothie')
+streamlit.text('\N{flexed biceps} Hard-Boiled Free-Range Egg')
+streamlit.text('\N{flexed biceps} Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
+import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.dataframe(my_fruit_list)
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+# Display the table on the page
+streamlit.dataframe(my_fruit_list)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+#new section to desplay Fuitverse api responce
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# streamlit.text(fruityvice_response.json())
+# Convert from json to normelize  
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output as table
+#streamlit.dataframe(fruityvice_normalized)
+#streamlit.header('Fruityvice Fruit Advice!') import requests
+import requests
+fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_repsonse = requests.get("https://fruityvice.com/api/fruit/"+"apple")
+fruityvice_normalized = pandas.json_normalize(fruityvice_repsonse.json())
+streamlit.dataframe(fruityvice_normalized)
